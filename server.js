2c175132447f608af7eb7bf3fcf6810e59f468a6
Pooja Chawla <pooja@Poojas-MacBook-Air.local>
2019-07-21 11:37:37 +0530

    build bcrypt

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ app.put('/image', (req,res)=>{image.handleImage(req,res,db)})
 app.post('/imageUrl', (req,res)=>{image.handleApiCall(req,res)})
 
 app.listen(process.env.PORT || 4000, ()=>{
-	console.log('app is running on port ${process.env.PORT}')
+	console.log(`app is running on port ${process.env.PORT}`)
 })
