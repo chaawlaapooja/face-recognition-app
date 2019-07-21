8206d85cd3a7ccc57146472682f1337275f1d4dd
Pooja Chawla <pooja@Poojas-MacBook-Air.local>
2019-07-21 05:40:07 +0530

    Updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image')
 const db = knex({
 	client:'pg',
 	connection:{
-		host:'127.0.0.1',
+		host:'postgresql-closed-57774',
 		user:'pooja',
 		password:'',
 		database:'smart-brain'
